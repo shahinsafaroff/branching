8952773a923a9bafdcf87078ef354fb30e5259e4
Shahin Safaroff <shahinsafaroff@gmail.com>
2020-07-10 19:57:48 +0400

    git-rebase 2

--- rebase.sh
+++ rebase.sh
@@ -3,7 +3,11 @@
 
 count=1
 for param in "$@"; do
+<<<<<<< HEAD
     echo "\$@ Parameter #$count = $param"
+=======
+    echo "Next parameter: $param"
+>>>>>>> ccea694... git-rebase 2
     count=$(( $count + 1 ))
 done
 
